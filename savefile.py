78e2099d98afbc60c642c31e88b59c70e1168760
Pedro Benevides <pedrobenevides@edu.unifor.br>
2018-11-01 19:10:57 -0300

    .

--- savefile.py
+++ savefile.py
@@ -6,9 +6,11 @@ import urllib.request
 from datetime import datetime
 from time import sleep
 import rospy
-from geometry_msgs import Twist
+#from geometry_msgs import Twist
 
-def salvarArquivos():
+#url = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/m1UI4XnQF7NZHBXdfRBDhVQheIz.jpg"
+url = "http://192.168.0.65/jpg/image.jpg"
+def salvarArquivos(x, z):
     log = open('Data/driver_log.csv', 'w')
     escrever = csv.writer(log)
     j = 0
@@ -18,7 +20,7 @@ def salvarArquivos():
         i += 1
     while b == 1:
         try:
-            urllib.request.urlretrieve("https://image.tmdb.org/t/p/w600_and_h900_bestv2/m1UI4XnQF7NZHBXdfRBDhVQheIz.jpg", "Data/IMG/imagem%s.jpg" % i)
+            urllib.request.urlretrieve(url, "Data/IMG/imagem%s.jpg" % i)
             nomeimagem = os.getcwd()+"/Data/IMG/imagem%s.jpg" % i
             print("imagem %s baixada com sucesso!" % i)
         except:
@@ -26,7 +28,7 @@ def salvarArquivos():
         #delay em segundos
         sleep(0.1)
         try:
-            escrever.writerow((nomeimagem, 'direção','angulo'))
+            escrever.writerow((nomeimagem, x,z))
             print("imagem %s salva no arquivo de Log com sucesso!" % i)
         except:
             print('ocorreu um erro para salvar a imagem %s' % i)
@@ -37,23 +39,6 @@ def salvarArquivos():
         
     print(os.getcwd())
     log.close()
-    
-def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("chatter", String, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-
-if __name__ == '__main__':
-    listener()
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
@@ -61,6 +46,7 @@ def callback(data):
     print('X: ', x)
     z = data.linear.z
     print('z: ', z)
+    salvarArquivos(x,z)
     
 def listener():
 
@@ -71,10 +57,13 @@ def listener():
     # run simultaneously.
     rospy.init_node('NodeDeTeste', anonymous=True)
 
+    #procura  um node com o nome definido atrás da mensagem Twist declada
+    #para executar a função callback
     rospy.Subscriber("drrobot_cmd_vel", Twist, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
